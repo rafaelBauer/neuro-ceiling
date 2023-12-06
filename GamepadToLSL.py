b53aab5c105178c83f99a7ce1a7a3bcc7253fb54
rafaelBauer <abauer.rafael@gmail.com>
2023-12-06 15:55:26 +0100

    Made gamepad to LSL

--- GamepadToLSL.py
+++ GamepadToLSL.py
@@ -1,4 +1,6 @@
 import logging
+
+import numpy as np
 import pylsl
 
 from neuroceiling import KeyboardObserver
@@ -7,29 +9,40 @@ from neuroceiling import KeyboardObserver
 class GamepadToLSL:
     def __init__(self):
         self.__gamepad: KeyboardObserver = KeyboardObserver()
+        self.__stream_outlet: pylsl.StreamOutlet = pylsl.StreamOutlet(pylsl.StreamInfo("Gamepad", "markers",
+                                                                                       8, pylsl.IRREGULAR_RATE,
+                                                                                       pylsl.cf_float32, "Gamepad",))
 
     def start(self) -> None:
         self.__gamepad.subscribe_callback_to_gripper(self.__gripper_callback)
         self.__gamepad.subscribe_callback_to_label(self.__label_callback)
         self.__gamepad.subscribe_callback_to_direction(self.__direction_callback)
         self.__gamepad.subscribe_callback_to_reset(self.__reset_callback)
-
-        # self.__stream_outlet: pylsl.StreamOutlet = pylsl.StreamOutlet(pylsl.StreamInfo("Gamepad", "Gamepad",
-        #                                                                                3, pylsl.cf_float32, "Gamepad"))
+        self.__gamepad.start()
 
     def stop(self) -> None:
         self.__gamepad.unsubscribe_callback_to_gripper(self.__gripper_callback)
         self.__gamepad.unsubscribe_callback_to_label(self.__label_callback)
         self.__gamepad.unsubscribe_callback_to_direction(self.__direction_callback)
+        self.__gamepad.stop()
 
     def __gripper_callback(self, gripper: float) -> None:
         print("Gripper: " + gripper.__str__())
+        data = [self.__gamepad.label, gripper]
+        data.extend(self.__gamepad.direction)
+        self.__stream_outlet.push_sample(data)
 
     def __label_callback(self, label: int) -> None:
         print("Label: " + label.__str__())
+        data = [label, self.__gamepad.gripper]
+        data.extend(self.__gamepad.direction)
+        self.__stream_outlet.push_sample(data)
 
     def __direction_callback(self, direction: [float]) -> None:
         print("Direction: " + direction.__str__())
+        data = [self.__gamepad.label, self.__gamepad.gripper]
+        data.extend(direction)
+        self.__stream_outlet.push_sample(data)
 
     def __reset_callback(self) -> None:
         print("Reset requested")
@@ -44,8 +57,13 @@ def main() -> None:
     gamepad_to_lsl: GamepadToLSL = GamepadToLSL()
     gamepad_to_lsl.start()
 
-    while True:
-        pass
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Stopping stream...")
+        gamepad_to_lsl.stop()
+
 
 if __name__ == '__main__':
     main()
--- neuroceiling/keyboardobserver.py
+++ neuroceiling/keyboardobserver.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import sys
 from typing import Callable, Optional
 import numpy as np
+from numpy import bool_
 from pynput import keyboard
 from functools import partial
 import time
@@ -19,7 +20,7 @@ class KeyboardObserver:
 
     def __init__(self):
         self.__label: int = 1
-        self.__gripper: Optional[float] = 0
+        self.__gripper: float = 0
         self.__direction: np.array = np.array([0, 0, 0, 0, 0, 0])
 
         self.__label_lock: threading.Lock = threading.Lock()
@@ -31,21 +32,16 @@ class KeyboardObserver:
                 "b": partial(self.__set_label, 0),  # bad
                 "c": partial(self.__set_gripper, -0.9),  # close
                 "v": partial(self.__set_gripper, 0.9),  # open
-                "f": partial(self.__set_gripper, None),  # gripper free
+                "f": partial(self.__set_gripper, 0),  # gripper free
                 "x": self.reset_episode,  # reset
             }
         )
-        self.__hotkeys.start()
-
-        # Need to sleep because apparently there is a race condition problem with one dependent
-        # library from pynput (pyobjc).
-        # The solution is described in this comment:
-        # https://github.com/moses-palmer/pynput/issues/55#issuecomment-1314410235
-        time.sleep(1)
 
         self.__listener = keyboard.Listener(
             on_press=self.__set_direction, on_release=self.__reset_direction
         )
+        self.__direction_keys_pressed: set = set()  # store which keys are pressed
+
         # Key mapping consists of (i, j), where:
         #   i = The index in the direction array
         #   j = The value of that index in the direction array
@@ -63,13 +59,27 @@ class KeyboardObserver:
             "u": (5, -1),  # rotate left
             "o": (5, 1),  # rotate right
         }
-        self.__listener.start()
+
 
         self.__label_callbacks: [Callable[[int], None]] = []
         self.__gripper_callbacks: [Callable[[float], None]] = []
         self.__direction_callbacks: [Callable[[np.array], None]] = []
         self.__reset_callbacks: [Callable[[], None]] = []
         return
+
+    def start(self) -> None:
+        self.__hotkeys.start()
+
+        # Need to sleep because apparently there is a race condition problem with one dependent
+        # library from pynput when running on macOS (pyobjc).
+        # The solution is described in this comment:
+        # https://github.com/moses-palmer/pynput/issues/55#issuecomment-1314410235
+        time.sleep(1)
+        self.__listener.start()
+
+    def stop(self) -> None:
+        self.__hotkeys.stop()
+        self.__listener.stop()
 
     def __set_label(self, value: int) -> None:
         with self.__label_lock:
@@ -89,7 +99,7 @@ class KeyboardObserver:
         with self.__label_lock:
             return self.__label
 
-    def __set_gripper(self, value: Optional[float]) -> None:
+    def __set_gripper(self, value: float) -> None:
         with self.__gripper_lock:
             self.__gripper = value
             self.__call_callbacks(self.__gripper_callbacks, self.__gripper)
@@ -111,8 +121,10 @@ class KeyboardObserver:
         with self.__direction_lock:
             try:
                 idx, value = self.__KEY_MAPPING[key.char]
-                self.__direction[idx] = value
-                self.__call_callbacks(self.__direction_callbacks, self.__direction)
+                if key not in self.__direction_keys_pressed:
+                    self.__direction[idx] = value
+                    self.__call_callbacks(self.__direction_callbacks, self.__direction)
+                    self.__direction_keys_pressed.add(key)
             except (KeyError, AttributeError):
                 pass
 
@@ -128,8 +140,11 @@ class KeyboardObserver:
         with self.__direction_lock:
             try:
                 idx, _ = self.__KEY_MAPPING[key.char]
+                old_direction_array = self.__direction.copy()
                 self.__direction[idx] = 0
-                self.__call_callbacks(self.__direction_callbacks, self.__direction)
+                if not np.array_equal(old_direction_array, self.__direction):
+                    self.__call_callbacks(self.__direction_callbacks, self.__direction)
+                self.__direction_keys_pressed.remove(key)
             except (KeyError, AttributeError):
                 pass
 
@@ -138,7 +153,7 @@ class KeyboardObserver:
         with self.__direction_lock:
             return self.__direction
 
-    def has_joints_cor(self):
+    def has_joints_cor(self) -> np.bool_:
         with self.__direction_lock:
             return self.__direction.any()
 
@@ -154,7 +169,7 @@ class KeyboardObserver:
         for callback in self.__reset_callbacks:
             callback()
         self.__set_label(1)
-        self.__set_gripper(None)
+        self.__set_gripper(0)
 
     def subscribe_callback_to_reset(self, callback_func: Callable[[], None]) -> None:
         self.__reset_callbacks.append(callback_func)
