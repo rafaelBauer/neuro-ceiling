590d6f9cf21a049fe633a8b3361387a359d8c949
Rafael <abauer.rafael@gmail.com>
2024-04-29 15:50:15 +0200

    10 Add pylint and black support (#12)
    * Added pylint and black formatter

--- src/utils/keyboard_observer.py
+++ src/utils/keyboard_observer.py
@@ -1,21 +1,29 @@
 """
-Class copied from CEILing project.
+Class based from CEILing project. Some extra features were added and functions renamed.
 https://github.com/robot-learning-freiburg/CEILing/blob/main/src/utils.py
 """
 from __future__ import annotations
-
-import sys
 from enum import Enum
-from typing import Callable, Optional
-import numpy as np
-from numpy import bool_
-from pynput import keyboard
+from collections.abc import Callable
 from functools import partial
 import time
 import threading
+from typing import Final
+
+import numpy as np
+from pynput import keyboard
 
 
 class HumanFeedback(Enum):
+    """
+    Enum class for human feedback.
+
+    Attributes:
+        CORRECTED: A human had to correct the action. Value is -1.
+        BAD: A human considered a bad action. Value is 0.
+        GOOD: A human considered a good action Value is 1.
+    """
+
     CORRECTED = -1
     BAD = 0
     GOOD = 1
@@ -23,12 +31,31 @@ class HumanFeedback(Enum):
 
 class KeyboardObserver:
     """
+    This class is used to observe keyboard inputs and map them to certain actions.
     """
 
+    # Key mapping consists of (i, j), where:
+    #   i = The index in the direction array
+    #   j = The value of that index in the direction array
+    __KEY_MAPPING: Final[dict[str, tuple[int, int]]] = {
+        "s": (0, 1),  # backward
+        "w": (0, -1),  # forward
+        "a": (1, 1),  # left
+        "d": (1, -1),  # right
+        "q": (2, 1),  # down
+        "e": (2, -1),  # up
+        "j": (3, -1),  # look left
+        "l": (3, 1),  # look right
+        "i": (4, -1),  # look up
+        "k": (4, 1),  # look down
+        "u": (5, -1),  # rotate left
+        "o": (5, 1),  # rotate right
+    }
+
     def __init__(self):
-        self.__label: int = 1
-        self.__gripper: float = 0
-        self.__direction: np.array = np.array([0, 0, 0, 0, 0, 0])
+        self.__label: HumanFeedback = HumanFeedback.GOOD
+        self.__gripper: float | None = None
+        self.__direction: np.array = np.zeros(6)
 
         self.__label_lock: threading.Lock = threading.Lock()
         self.__gripper_lock: threading.Lock = threading.Lock()
@@ -39,39 +66,19 @@ class KeyboardObserver:
                 "b": partial(self.__set_label, HumanFeedback.BAD),  # bad
                 "c": partial(self.__set_gripper, -0.9),  # close
                 "v": partial(self.__set_gripper, 0.9),  # open
-                "f": partial(self.__set_gripper, 0),  # gripper free
+                "f": partial(self.__set_gripper, None),  # gripper free
                 "x": self.reset_episode,  # reset
             }
         )
-
         self.__listener = keyboard.Listener(
             on_press=self.__set_direction, on_release=self.__reset_direction
         )
         self.__direction_keys_pressed: set = set()  # store which keys are pressed
 
-        # Key mapping consists of (i, j), where:
-        #   i = The index in the direction array
-        #   j = The value of that index in the direction array
-        self.__KEY_MAPPING = {
-            "s": (0, 1),  # backward
-            "w": (0, -1),  # forward
-            "a": (1, 1),  # left
-            "d": (1, -1),  # right
-            "q": (2, 1),  # down
-            "e": (2, -1),  # up
-            "j": (3, -1),  # look left
-            "l": (3, 1),  # look right
-            "i": (4, -1),  # look up
-            "k": (4, 1),  # look down
-            "u": (5, -1),  # rotate left
-            "o": (5, 1),  # rotate right
-        }
-
         self.__label_callbacks: [Callable[[int], None]] = []
         self.__gripper_callbacks: [Callable[[float], None]] = []
         self.__direction_callbacks: [Callable[[np.array], None]] = []
         self.__reset_callbacks: [Callable[[], None]] = []
-        return
 
     def start(self) -> None:
         self.__hotkeys.start()
@@ -96,30 +103,36 @@ class KeyboardObserver:
         with self.__label_lock:
             self.__label_callbacks.append(callback_func)
 
-    def unsubscribe_callback_to_label(self, callback_func: Callable[[int], None]) -> None:
+    def unsubscribe_callback_to_label(
+        self, callback_func: Callable[[int], None]
+    ) -> None:
         with self.__label_lock:
             self.__label_callbacks.remove(callback_func)
 
     @property
-    def label(self) -> int:
+    def label(self) -> HumanFeedback:
         with self.__label_lock:
             return self.__label
 
-    def __set_gripper(self, value: float) -> None:
+    def __set_gripper(self, value: float | None) -> None:
         with self.__gripper_lock:
             self.__gripper = value
             self.__call_callbacks(self.__gripper_callbacks, self.__gripper)
 
-    def subscribe_callback_to_gripper(self, callback_func: Callable[[float], None]) -> None:
+    def subscribe_callback_to_gripper(
+        self, callback_func: Callable[[float], None]
+    ) -> None:
         with self.__gripper_lock:
             self.__gripper_callbacks.append(callback_func)
 
-    def unsubscribe_callback_to_gripper(self, callback_func: Callable[[float], None]) -> None:
+    def unsubscribe_callback_to_gripper(
+        self, callback_func: Callable[[float], None]
+    ) -> None:
         with self.__gripper_lock:
             self.__gripper_callbacks.remove(callback_func)
 
     @property
-    def gripper(self) -> float:
+    def gripper(self) -> float | None:
         with self.__gripper_lock:
             return self.__gripper
 
@@ -130,7 +143,7 @@ class KeyboardObserver:
         :return: If there is any gripper direction command, otherwise False
         """
         with self.__gripper_lock:
-            return abs(self.__gripper - 0) <= 0.000001
+            return self.__gripper is not None
 
     def __set_direction(self, key) -> None:
         with self.__direction_lock:
@@ -143,11 +156,15 @@ class KeyboardObserver:
             except (KeyError, AttributeError):
                 pass
 
-    def subscribe_callback_to_direction(self, callback_func: Callable[[np.array], None]) -> None:
+    def subscribe_callback_to_direction(
+        self, callback_func: Callable[[np.array], None]
+    ) -> None:
         with self.__direction_lock:
             self.__direction_callbacks.append(callback_func)
 
-    def unsubscribe_callback_to_direction(self, callback_func: Callable[[np.array], None]) -> None:
+    def unsubscribe_callback_to_direction(
+        self, callback_func: Callable[[np.array], None]
+    ) -> None:
         with self.__direction_lock:
             self.__direction_callbacks.remove(callback_func)
 
@@ -177,14 +194,6 @@ class KeyboardObserver:
         with self.__direction_lock:
             return np.count_nonzero(self.__direction) != 0
 
-    # def has_joints_cor(self) -> np.bool_:
-    #     with self.__direction_lock:
-    #         return self.__direction.any()
-    #
-    # def has_gripper_update(self) -> bool:
-    #     with self.__gripper_lock:
-    #         return self.gripper is not None
-
     def get_ee_action(self) -> np.array:
         with self.__direction_lock:
             return self.direction * 0.9
@@ -200,6 +209,6 @@ class KeyboardObserver:
 
     @classmethod
     def __call_callbacks(cls, callbacks, value) -> None:
-        if callbacks.__len__() != 0:
+        if len(callbacks) != 0:
             for callback in callbacks:
                 callback(value)
