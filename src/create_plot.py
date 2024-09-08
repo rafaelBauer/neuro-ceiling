cdcf269122dd728ffa34ce777a753a6e18ed40b4
rafaelBauer <abauer.rafael@gmail.com>
2024-09-08 14:14:08 +0200

    Combined error bands with plot line

--- src/create_plot.py
+++ src/create_plot.py
@@ -1,3 +1,4 @@
+import os
 from collections import defaultdict
 
 import matplotlib.pyplot as plt
@@ -5,10 +6,14 @@ from scipy.interpolate import make_interp_spline
 import pandas
 import numpy
 
+plt.rcParams.update({"text.usetex": True, "font.family": "sans-serif", "font.sans-serif": ["Helvetica"]})
+
 legend = []
+plots = []
+
 
 def plot_csv_data(file_input, data_name):
-    csv_input = pandas.read_csv(f'../data/{file_input}')
+    csv_input = pandas.read_csv(f"../data/{file_input}")
     data = defaultdict(list)
     order = defaultdict(int)
     legend.append(data_name)
@@ -18,7 +23,9 @@ def plot_csv_data(file_input, data_name):
         split_model_name = model_name.split("_")
         corrective_index = split_model_name.index("corr") - 1
         evaluative_index = split_model_name.index("eval") - 1
-        feedback_ratio = f'Corrective: {split_model_name[corrective_index]}%, Evaluative: {split_model_name[evaluative_index]}%'
+        feedback_ratio = (
+            f"Corrective: {split_model_name[corrective_index]}%, Evaluative: {split_model_name[evaluative_index]}%"
+        )
         order[feedback_ratio] = int(split_model_name[corrective_index])
         data[feedback_ratio].append(success_rate)
 
@@ -26,7 +33,7 @@ def plot_csv_data(file_input, data_name):
     data = {key: data[key] for key, value in sorted(order.items(), key=lambda item: item[1])}
     order = {key: value for key, value in sorted(order.items(), key=lambda item: item[1])}
 
-    data = pandas.DataFrame.from_dict(data, orient='index')
+    data = pandas.DataFrame.from_dict(data, orient="index")
     std = data.std(axis=1)
     mean = data.mean(axis=1)
 
@@ -38,21 +45,31 @@ def plot_csv_data(file_input, data_name):
     X_ = numpy.linspace(ratio_array.min(), ratio_array.max(), 500)
     Y_ = smooth_mean(X_)
     error = smooth_std(X_)
-
-    plt.plot(X_, Y_)
-    plt.fill_between(X_, Y_ - error, Y_ + error, alpha=0.2)
-    plt.ylabel('Success Rate', fontsize=14)
-    plt.xlabel('Corrective Feedback Ratio', fontsize=14)
-    plt.ylim(0, 1)
+    plot = plt.plot(X_, Y_)
+    error_band = plt.fill_between(X_, Y_ - error, Y_ + error, alpha=0.2)
+    plots.append((plot[0], error_band))
 
 
-plot_title = 'CEILing with Penalized Actions'
-graph_titles = ['CEILing with Penalized Actions']
-csv_files = ['ceiling_penalized_actions.csv']
+# plot_title = 'Original CEILing'
+# graph_titles = ['Original CEILing']
+# csv_files = ['original_ceiling.csv']
+
+plot_title = "CEILing with Penalized Actions"
+graph_titles = ["Original CEILing", "CEILing with Penalized Actions"]
+csv_files = ["original_ceiling.csv", "ceiling_penalized_actions.csv"]
 
 for csv_file, graph_title in zip(csv_files, graph_titles):
     plot_csv_data(csv_file, graph_title)
 
 plt.title(plot_title, fontsize=18)
-plt.legend(legend)
+plt.ylim(0, 1)
+plt.ylabel("Success Rate", fontsize=12)
+plt.xlabel("Corrective Feedback Ratio [\%]", fontsize=12)
+plt.legend(plots, legend)
+
+image_name = plot_title.replace(" ", "_").lower()
+image_path = "../data/output/"
+if not os.path.isdir(image_path):
+    os.makedirs(image_path)
+plt.savefig(os.path.join(image_path, f"{image_name}.pdf"))
 plt.show()
